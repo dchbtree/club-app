520c55c904ed1171a647c65fdd1b61b478e0a68c
dchbtree <164089618+dchbtree@users.noreply.github.com>
2024-03-19 22:04:01 +0000

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import './App.css';
+import { useEffect } from "react";
+import { supabase } from "./supabaseClient";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+export default function App() {
+  async function loadData() {
+    console.log("loading data");
+    const { data, error } = await supabase.from("items").select("*");
+    console.log(data, error);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return <div>Club App</div>;
 }
-
-export default App;
